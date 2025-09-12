9253e1f17ad36808efb57cdcf4ad8177e1a19457
rana-asaireh <rana.asaireh@gmail.com>
2025-09-13 01:59:27 +0300

    editing 4

--- src/app.js
+++ src/app.js
@@ -7,8 +7,19 @@ import tasksRoutes from './modules/tasks/tasksRoutes.js';
 
 const app = express();
 
+const allowedOrigins = [
+  'https://to-do-frontend-mu-plum.vercel.app',
+  'https://to-do-frontend-lg4xxg1jz-rana-asairehs-projects.vercel.app'
+];
 app.use(cors({
-  origin: 'https:to-do-frontend-lg4xxg1jz-rana-asairehs-projects.vercel.app',
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 }));
 
