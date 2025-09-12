38ac347477d157a613ac5ba8e8c60f9719b548b5
rana-asaireh <rana.asaireh@gmail.com>
2025-09-13 01:52:11 +0300

    editing 2

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import tasksRoutes from './modules/tasks/tasksRoutes.js';
 const app = express();
 
 app.use(cors({
-  origin: 'https:to-do-frontend-9ysgdjycr-rana-asairehs-projects.vercel.app',
+  origin: 'https://to-do-frontend-mu-plum.vercel.app/',
   credentials: true,
 }));
 
