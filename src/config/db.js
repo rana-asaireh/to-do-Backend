0951365b8b0169f83ef76a3c52a298c6ed3ca101
rana-asaireh <rana.asaireh@gmail.com>
2025-09-13 15:27:17 +0300

    connecting with db

--- src/config/db.js
+++ src/config/db.js
@@ -13,8 +13,32 @@
 //     dialect: dbConfig.dialect,
 //   }
 // );
-import config from './config.js';
+// import config from './config.js';
+// import { Sequelize } from 'sequelize';
+
+// const env = process.env.NODE_ENV || 'production';
+// const dbConfig = config[env];
+
+// export const sequelize = new Sequelize(
+//   dbConfig.database,
+//   dbConfig.username,
+//   dbConfig.password,
+//   {
+//     host: dbConfig.host,
+//     port: parseInt(dbConfig.port), 
+//     dialect: dbConfig.dialect,
+//     logging: false,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false, 
+//       },
+//     },
+//   }
+// );
+import fs from 'fs';
 import { Sequelize } from 'sequelize';
+import config from './config/config.js';
 
 const env = process.env.NODE_ENV || 'production';
 const dbConfig = config[env];
@@ -25,14 +49,14 @@ export const sequelize = new Sequelize(
   dbConfig.password,
   {
     host: dbConfig.host,
-    port: parseInt(dbConfig.port), 
+    port: parseInt(dbConfig.port),
     dialect: dbConfig.dialect,
     logging: false,
     dialectOptions: {
       ssl: {
-        require: true,
-        rejectUnauthorized: false, 
+        ca: fs.readFileSync(process.env.DB_SSL_CA_PATH),
       },
+      connectTimeout: 10000, 
     },
   }
 );
