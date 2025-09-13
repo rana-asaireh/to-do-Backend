da8d560debc6f71caefdecdd8044de59a307d9a7
rana-asaireh <rana.asaireh@gmail.com>
2025-09-13 20:20:46 +0300

    update app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import tasksRoutes from './modules/tasks/tasksRoutes.js';
 
 const app = express();
 
+// TRUST PROXY: needed for secure cookies behind Render proxy
+app.set('trust proxy', 1);
+
 // const allowedOrigins = [
 //   'https://to-do-frontend-mu-plum.vercel.app',
 //   'https://to-do-frontend-lg4xxg1jz-rana-asairehs-projects.vercel.app'
@@ -23,11 +26,27 @@ const app = express();
 //   },
 //   credentials: true,
 // }));
-const allowedOrigins = [
-  'https://to-do-frontend-mu-plum.vercel.app',
-  'https://to-do-frontend-lg4xxg1jz-rana-asairehs-projects.vercel.app'
-];
 
+
+// const allowedOrigins = [
+//   'https://to-do-frontend-mu-plum.vercel.app',
+//   'https://to-do-frontend-lg4xxg1jz-rana-asairehs-projects.vercel.app'
+// ];
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin) return callback(null, true); // allow Postman / server requests
+//     const normalizedOrigin = origin.replace(/\/$/, '');
+//     if (allowedOrigins.some(url => url.replace(/\/$/, '') === normalizedOrigin)) {
+//       return callback(null, true);
+//     }
+//     return callback(new Error(`CORS error: origin ${origin} not allowed`), false);
+//   },
+//   credentials: true, 
+// }));
+
+// // Preflight OPTIONS requests
+// app.options('*', cors({ origin: allowedOrigins, credentials: true }));
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true); // allow Postman / server requests
@@ -37,11 +56,20 @@ app.use(cors({
     }
     return callback(new Error(`CORS error: origin ${origin} not allowed`), false);
   },
-  credentials: true, 
+  credentials: true,
 }));
 
-// Preflight OPTIONS requests
-app.options('*', cors({ origin: allowedOrigins, credentials: true }));
+app.options('*', cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    const normalizedOrigin = origin.replace(/\/$/, '');
+    if (allowedOrigins.some(url => url.replace(/\/$/, '') === normalizedOrigin)) {
+      return callback(null, true);
+    }
+    return callback(new Error(`CORS error: origin ${origin} not allowed`), false);
+  },
+  credentials: true,
+}));
 
 
 
