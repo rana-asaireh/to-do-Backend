514414ecbc2a39accbc133b7342024f7f45cb9a5
rana-asaireh <rana.asaireh@gmail.com>
2025-09-13 15:46:46 +0300

    t

--- src/config/db.js
+++ src/config/db.js
@@ -54,7 +54,7 @@ export const sequelize = new Sequelize(
     logging: false,
     dialectOptions: {
       ssl: {
-        ca: fs.readFileSync('./ca.pem'),
+        ca: fs.readFileSync('/etc/secrets/ca.pem'),
       },
       connectTimeout: 20000, 
     },
